2edf89b2e648fca40caf369f341d419a7f4470ec
Jeffrey Rose <jtr219@outlook.com>
2021-12-03 22:44:03 -0500

    fix navbar menu visibility on mobile

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,33 +1,41 @@
 import Link from "next/link";
+import { useState } from "react";
 
-const NavBarLinks = () => {
+const NavBarLinks = (props: { visible: boolean }) => {
   return (
-    <ul className="navbar-nav ms-auto py-4 py-lg-0">
-      <li className="nav-item">
-        <Link href="/">
-          <a className="nav-link px-lg-3 py-3 py-lg-4">Home</a>
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link href="/about">
-          <a className="nav-link px-lg-3 py-3 py-lg-4">About</a>
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link href="/post">
-          <a className="nav-link px-lg-3 py-3 py-lg-4">Sample Post</a>
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link href="/contact">
-          <a className="nav-link px-lg-3 py-3 py-lg-4">Contact</a>
-        </Link>
-      </li>
-    </ul>
+    <div
+      className="collapse navbar-collapse"
+      id="navbarResponsive"
+      style={{ display: props.visible ? "block" : "none" }}
+    >
+      <ul className="navbar-nav ms-auto py-4 py-lg-0">
+        <li className="nav-item">
+          <Link href="/">
+            <a className="nav-link px-lg-3 py-3 py-lg-4">Home</a>
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link href="/about">
+            <a className="nav-link px-lg-3 py-3 py-lg-4">About</a>
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link href="/post">
+            <a className="nav-link px-lg-3 py-3 py-lg-4">Sample Post</a>
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link href="/contact">
+            <a className="nav-link px-lg-3 py-3 py-lg-4">Contact</a>
+          </Link>
+        </li>
+      </ul>
+    </div>
   );
 };
 
 export const NavBar = () => {
+  const [visible, setVisible] = useState(false);
   return (
     <nav className="navbar navbar-expand-lg navbar-light" id="mainNav">
       <div className="container px-4 px-lg-5">
@@ -42,13 +50,12 @@ export const NavBar = () => {
           aria-controls="navbarResponsive"
           aria-expanded="false"
           aria-label="Toggle navigation"
+          onClick={() => setVisible(!visible)}
         >
           Menu
           <i className="fas fa-bars"></i>
         </button>
-        <div className="collapse navbar-collapse" id="navbarResponsive">
-          <NavBarLinks />
-        </div>
+        <NavBarLinks visible={visible} />
       </div>
     </nav>
   );
