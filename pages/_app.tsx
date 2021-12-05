698fd28dd404c001901720ec2742edbbd5946a2c
Jeffrey Rose <jtr219@outlook.com>
2021-12-04 23:36:41 -0500

    remove unneeded comments

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,8 @@
 // add bootstrap css
 import "bootstrap/dist/css/bootstrap.css";
-
 import "@fortawesome/fontawesome-free/css/all.css";
 // own css files here
 import "../styles/styles.scss";
-//import "../styles/tst.scss";
 import Head from "next/head";
 import type { AppProps } from "next/app";
 import { Footer } from "../components/Footer";
